4b7149703489940b2f04711bc6d22945c3875976
wocanmei <pingao777@gmail.com>
2019-01-06 16:04:00 +0800

    可选作业

--- machine-learning-ex4/ex4/ex4.m
+++ machine-learning-ex4/ex4/ex4.m
@@ -230,5 +230,48 @@ pause;
 pred = predict(Theta1, Theta2, X);
 
 fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y)) * 100);
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
+
+%% ================= Part 11: Optional Execise =================
+lambdas = [0 0.01 0.03 0.09 0.1 0.3, 0.9];
+accuracy = zeros(length(lambda), 1);
+options = optimset('MaxIter', 200);
+initial_nn_params = [initial_Theta1(:) ; initial_Theta2(:)];
+for l = 1:length(lambdas)
+    %  You should also try different values of lambda
+    lambda = lambdas(l);
+    
+    % Create "short hand" for the cost function to be minimized
+    costFunction = @(p) nnCostFunction(p, ...
+        input_layer_size, ...
+        hidden_layer_size, ...
+        num_labels, X, y, lambda);
+    
+    % Now, costFunction is a function that takes in only one argument (the
+    % neural network parameters)
+    [nn_params, cost] = fmincg(costFunction, initial_nn_params, options);
+    
+    % Obtain Theta1 and Theta2 back from nn_params
+    Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
+        hidden_layer_size, (input_layer_size + 1));
+    
+    Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
+        num_labels, (hidden_layer_size + 1));
+    
+    pred = predict(Theta1, Theta2, X);
+    accuracy(l) = mean(double(pred == y)) * 100;
+    fprintf('\nTraining Set Accuracy(lambda=%f): %f\n', lambda, accuracy(l));
+end
+plot(lambdas, accuracy, '-');
+xlabel('lambda');
+ylabel('accuracy(%)');
+title('Trainning accuracy by different lambda(MaxIter=200)');
+savePlot('../../figure/ex4-1 Trainning accuracy by different lambda(MaxIter=200).png');
+
+
+
+
 
 
