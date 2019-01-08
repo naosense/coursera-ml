1e3454417fe1ee632cd22876788d3e7ec4590687
wocanmei <pingao777@gmail.com>
2019-01-08 17:53:56 +0800

    保存图片增加是否覆盖选项

--- common/savePlot.m
+++ common/savePlot.m
@@ -1,4 +1,9 @@
 function savePlot(path)
+    overwrite = false;
+    if (!overwrite & exist(path, 'file'))
+        return;
+    end
+    
     PRINT_TO_FILE = true;
     if (PRINT_TO_FILE) 
         print('-dpng', path, '-r72');
